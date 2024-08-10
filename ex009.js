bd0156e3aa59fa54adf0015c453d5a1023b3e4b6
Bruno <bruno_oliuza2008@hotmail.com>
2024-08-10 20:36:46 -0300

    Add exercises

---
+++ ex009.js
@@ -0,0 +1,7 @@
+var a = 60
+console.log(`A sua velocidade é de ${a}Km/h`)
+if(a > 50){
+    console.log(`Você foi multado, a velocidade máxima é de 50Km/h`)
+}else{
+    console.log(`Dirija sempre com cinto de segurança`)
+}
